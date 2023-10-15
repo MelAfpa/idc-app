0715c1920146a0ba5fa7b3653065af3a36ebaf6d
Melissa <you@example.com>
2023-10-15 16:57:35 +0200

    TOujours sur la page d'accueil

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <a href="#">Qui sommes nous ?</a>
+        <a href="#">La Pension</a>
+        <a href="#">Adopter</a>
+        <a href="#">Parrainer</a>
+        <a href="#">Bénévolat</a>
+        <a href="#">Faire un don</a>
       </header>
-      <section className='infos'>
+
+      <section className='App-infos'>
         <h1>Refuge ouvert tous les jours de 14h à 18h sauf le dimanche !</h1>
-        <h2>Découvrez les actualités du moment</h2>
-        <button>Juste ici</button>
+        <a href="#"> Découvrez les actualités du moment</a>
+        <p className='address'>Refuge Canin Un Cœur Sans Toit, Chemin du Clos Melon, Lieu dit « Maffron », 49700 Doué la Fontaine 
+        </p>
+        <p className='tel'>Téléphone : 06 15 29 09 56</p>
       </section>
-      <footer className='home_footer'></footer>
+
+      <footer className='App-footer'></footer>
     </div>
   );
 }
