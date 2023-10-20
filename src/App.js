294577d5052160f28a217eced19881d487e91614
Melissa <you@example.com>
2023-10-20 17:58:56 +0200

    carte en cours d'ajout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './uploads/banniere2.png';
+import logo from './uploads/banniere.png';
 import './App.css';
+// import './home/Home';
+import { FaFacebook, FaCommentAlt } from "react-icons/fa";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="#">Qui sommes nous ?</a>
-        <a href="#">La Pension</a>
-        <a href="#">Adopter</a>
-        <a href="#">Parrainer</a>
-        <a href="#">Bénévolat</a>
-        <a href="#">Faire un don</a>
+        <a href="App.js" className='home'><img src={logo} href="App.js" className="App-logo" alt="logo" /></a>
+        <a href="#" className='team'>Qui sommes nous ?</a>
+        <a href="#" className='pension'>La Pension</a>
+        <a href="#" className='adoption'>Adopter</a>
+        <a href="#" className='sponsor'>Parrainer</a>
+        <a href="#" className='volunteer'>Bénévolat</a>
+        <a href="#" className='donation'>Faire un don</a>
       </header>
 
       <section className='App-infos'>
@@ -22,7 +26,26 @@ function App() {
         <p className='tel'>Téléphone : 06 15 29 09 56</p>
       </section>
 
-      <footer className='App-footer'></footer>
+      <section className='map'>
+        <MapContainer center={[47.192, -0.249]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* <Marker position={[47.192, -0.249]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker> */}
+        </MapContainer>
+        
+      </section>
+
+      <footer className='App-footer'>
+        <a href="#" className='fb'><FaFacebook/></a>
+        <a href="#" className='contact'><FaCommentAlt/></a>
+        <a href="#" className='mentions'>Mentions légales</a>
+      </footer>
     </div>
   );
 }
