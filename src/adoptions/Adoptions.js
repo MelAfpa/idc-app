195e5d1f1de152d65b7e989aeebaf8b67ca062da
Melissa <you@example.com>
2023-11-01 21:46:22 +0100

    déplacement du fichier adoption

---
+++ src/adoptions/Adoptions.js
@@ -0,0 +1,20 @@
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+
+
+
+function Adoption() {
+    console.log("Adoption");
+
+    return(
+        <div className='adoption'>
+            <Header></Header>
+            <section>
+                <h1>Adoption</h1>
+            </section>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Adoption;
