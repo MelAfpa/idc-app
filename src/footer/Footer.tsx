e1562cdcc6af70bac2df92b48fe176721f5f18d4
Melissa <you@example.com>
2023-11-23 22:24:20 +0100

    mise en forme react

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import './Footer.css';
+// import './Footer.css';
 import { FaFacebook, FaCommentAlt } from 'react-icons/fa';
+import { Flex, Link } from '@chakra-ui/react';
 
 function Footer() {
   console.log('FOOTER');
 
   return (
-    <footer className="App-footer">
-      <a href="https://www.facebook.com/UnCoeurSansToit" className="fb">
+    <Flex
+      justifyContent={'space-evenly'}
+      alignItems={'center'}
+      p={'10px'}
+      mt={'10px'}
+      fontSize={'1.2em'}
+    >
+      <Link href="https://www.facebook.com/UnCoeurSansToit" className="fb">
         <FaFacebook color="#8ead76" size={40} />
-      </a>
-      <a href="#" className="contact">
+      </Link>
+      <Link href="#" className="contact">
         <FaCommentAlt color="#8ead76" size={40} />
-      </a>
-      <a href="#" className="mentions">
+      </Link>
+      <Link href="#" className="mentions" color="#8ead76">
         Mentions légales
-      </a>
-    </footer>
+      </Link>
+    </Flex>
   );
 }
 
