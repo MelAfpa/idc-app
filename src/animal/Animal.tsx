585e3538dea94bb26186ba4b8cb2ab055595ade7
Melissa <you@example.com>
2023-11-23 17:38:19 +0100

    Création de carte pour présenter les chiens à adopter

--- src/animal/Animal.tsx
+++ src/animal/Animal.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { IAnimal } from 'src/types/Animal';
+import './Animal.css';
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+import { FaBaby, FaCat, FaDog } from 'react-icons/fa';
 
 interface IProps {
   animal: IAnimal;
@@ -7,5 +10,26 @@ interface IProps {
 
 export function Animal({ animal }: IProps) {
   //   console.log(props);
-  return <div>{animal.name}</div>;
+  return (
+    <>
+      <Card className="animalCard" maxW="sm">
+        <CardHeader className="cardHeader">
+          <img
+            className="animalPicture"
+            src={animal.picture}
+            alt={animal.name}
+          />
+        </CardHeader>
+        <CardBody className="cardBody">
+          <header className="cardBodyHeader">{animal.name}</header>
+          <body className="cardBodyBody">{animal.description}</body>
+        </CardBody>
+        <CardFooter className="cardFooter">
+          {animal.cat_ok && <FaCat />}
+          {animal.dog_ok && <FaDog />}
+          {animal.baby_ok && <FaBaby />}
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
