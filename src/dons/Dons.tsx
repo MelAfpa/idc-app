0fd6532c26dda2c0a502392b7b5becbd5d54b0da
Melissa <you@example.com>
2023-12-06 13:57:10 +0100

    Creation de la page Dons

---
+++ src/dons/Dons.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../footer/Footer';
+import Header from 'src/header/Header';
+
+export default function Dons() {
+  return (
+    <>
+      <Header />
+      <div>Dons</div>
+      <Footer />
+    </>
+  );
+}
