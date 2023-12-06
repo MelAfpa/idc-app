b069ff9b8509a305cd0855579c3078435ddca8d3
Melissa <you@example.com>
2023-12-06 13:57:25 +0100

    Creation de la page Benevolat

---
+++ src/benevolat/Benevolat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../footer/Footer';
+import Header from 'src/header/Header';
+
+export default function Benevolat() {
+  return (
+    <>
+      <Header />
+      <div>benevolat</div>
+      <Footer />
+    </>
+  );
+}
